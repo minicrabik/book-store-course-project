fde00963c20c6bc2d3ea6d1de9fde913653954f7
minicrabik <agl.malcv@gmail.com>
2025-12-19 22:17:09 +0300

    feat: registered all 14 models in django admin with list displays

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Author, Genre, Publisher, Section, Book, BookAuthor, BookGenre, BookSection
 
-# Register your models here.
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name')
+    search_fields = ('last_name',)
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'publisher') 
+    search_fields = ('title',)
+
+@admin.register(Publisher)
+class PublisherAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Section)
+class SectionAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+admin.site.register(BookAuthor)
+admin.site.register(BookGenre)
+admin.site.register(BookSection)
--- sales/admin.py
+++ sales/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import Employee, Customer, Supplier, Sale, Receipt, SalePosition, ReceiptPosition
 
-# Register your models here.
+# Настройка отображения сотрудников
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'position', 'salary', 'phone')
+    list_filter = ('position',)
+    search_fields = ('last_name', 'passport')
+
+# Настройка отображения покупателей
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'phone')
+    search_fields = ('last_name', 'phone')
+
+# Настройка отображения продаж
+@admin.register(Sale)
+class SaleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'sale_date', 'employee', 'customer')
+    list_filter = ('sale_date', 'employee')
+
+# Настройка отображения поступлений
+@admin.register(Receipt)
+class ReceiptAdmin(admin.ModelAdmin):
+    list_display = ('id', 'receipt_date', 'supplier', 'publisher')
+    list_filter = ('receipt_date',)
+
+# Регистрация позиций (состав чеков и накладных)
+@admin.register(SalePosition)
+class SalePositionAdmin(admin.ModelAdmin):
+    list_display = ('sale', 'book', 'quantity', 'price')
+
+@admin.register(ReceiptPosition)
+class ReceiptPositionAdmin(admin.ModelAdmin):
+    list_display = ('receipt', 'book', 'quantity', 'purchase_price')
+
+admin.site.register(Supplier)
