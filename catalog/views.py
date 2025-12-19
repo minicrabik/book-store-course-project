8da36aaf923a34d72920267094fa4a3a5f983ec8
minicrabik <agl.malcv@gmail.com>
2025-12-19 22:57:48 +0300

    feat: display book authors on the main page with modern UI

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Book, BookAuthor
 
-# Create your views here.
+def index(request):
+    # Получаем книги и сразу подтягиваем связанные данные для скорости
+    books = Book.objects.all().select_related('publisher')
+    
+    # Для каждой книги найдем ее авторов
+    for book in books:
+        # Получаем список имен авторов через связующую таблицу
+        authors = BookAuthor.objects.filter(book=book).select_related('author')
+        book.author_list = ", ".join([f"{a.author.first_name} {a.author.last_name}" for a in authors])
+    
+    return render(request, 'catalog/index.html', {'books': books})
